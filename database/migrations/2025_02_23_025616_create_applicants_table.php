c719b60df3dbf05c1f0782347e4b28f51b210b2a
Ned <raidenmonsalud@gmail.com>
2025-02-26 07:21:14 +0800

    Backend forms

--- database/migrations/2025_02_23_025616_create_applicants_table.php
+++ database/migrations/2025_02_23_025616_create_applicants_table.php
@@ -4,36 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+return new class extends Migration {
+    public function up()
     {
-            Schema::create('applicants', function (Blueprint $table) {
-                $table->id();
-                $table->string('first_name', 100);
-                $table->string('last_name', 100);
-                $table->string('email', 150)->unique();
-                $table->string('phone_number', 20);
-                $table->text('address');
-                $table->string('resume', 255); // File path of uploaded CV
-                $table->boolean('privacy_policy_agreed')->default(false);
-                $table->enum('status', ['pending', 'screening','scheduled', 'interviewed', 'hired', 'rejected','archived'])->default('pending');
-                $table->timestamp('applied_at')->useCurrent();
-                $table->string('ip_address', 45)->nullable();
-                $table->string('user_agent', 255)->nullable();
-                $table->foreignId('job_id')->constrained('job_posts')->onDelete('cascade')->onUpdate('cascade'); // Foreign key
-
-                $table->timestamps(); // Laravel default created_at and updated_at
-            });
+        Schema::create('applicants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('job_id')->constrained('job_posts')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->text('address');
+            $table->string('cv'); // File path
+            $table->boolean('privacy_policy_agreed')->default(0);
+            $table->enum('status', ['pending', 'screening', 'scheduled', 'interviewed', 'hired', 'rejected', 'archived'])->default('pending');
+            $table->timestamp('applied_at')->useCurrent();
+            $table->timestamps();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('applicants');
     }
